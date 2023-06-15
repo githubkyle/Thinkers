980a82282e91ad812e1797df75d1cf7437d4fcb4
Kyle Schwent <43896355+githubkyle@users.noreply.github.com>
2023-06-15 00:09:07 -0500

    Update thoughts.js

--- Routes/api/thoughts.js
+++ Routes/api/thoughts.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const app = express();
-// app.use(express.json());
-// const mongoose = require("mongoose");
-// let db = mongoose.connect("mongodb://localhost/thought-network-db");
-
-// app.post("/create", (req, res) => {
-//   const { thoughtText, username } = req.body;
-
-//   db.collection("thoughtCollection")
-//     .insertOne({ thoughtText, username, createdAt: new Date() })
-//     // Sends results
-//     .then(result => {
-//       res.status(201).json(result.ops[0]);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({ error: "Failed to create that thought" });
-//     });
-// });
-
-// app.get("/see", (req, res) => {
-//   db.collection("thoughtCollection")
-//     .find({})
-//     .toArray()
-//     .then(results => res.json(results))
-//     .catch(err => {
-//       if (err) throw err;
-//     });
-// });
 const router = require("express").Router();
 
 const {

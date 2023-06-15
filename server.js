11bea47fac0f107e94fa5c37731bfa0eff0a237a
Kyle Schwent <43896355+githubkyle@users.noreply.github.com>
2023-06-15 00:06:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-// Import Mongoose models
-// const users = require("./models/User");
-// const thoughts = require("./models/Thought");
+const users = require("./models/Users");
+const thoughts = require("./models/Thought");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +13,9 @@ app.use(express.json());
 app.use(routes);
 
 mongoose.connect("mongodb://localhost/thought-network-db");
+
+users.syncIndexes();
+thoughts.syncIndexes();
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to the database");
